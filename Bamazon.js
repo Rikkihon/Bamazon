4e3257eaff62236cda02b5f66f3d1f2b07535bc9
Rikki Honnold <rikki@rikkis-mbp.lan>
2018-05-08 16:51:20 -0500

    update

--- Bamazon.js
+++ Bamazon.js
@@ -1,27 +1,3 @@
-// Create a MySQL Database called bamazon.
-// Then create a Table inside of that database called products.
-// The products table should have each of the following columns:
-
-// Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale.
-// Include the ids, names, and prices of products for sale.
-// The app should then prompt users with two messages.
-
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -29,7 +5,7 @@ var connection;
 
 var itemToOrder;
 var orderItem;
-
+var itemPrice;
 
 function openDatabase() {
   connection = mysql.createConnection({
@@ -87,24 +63,23 @@ function orderNumber() {
         console.log(answer.stockQuantity);//this is the quantity the customer wants
         console.log("      ");
         console.log("Your order is being placed. Please wait a few minutes while Bamazon processes it.")
-        var cost = (answer.stockQuantity * itemPrice);
-        console.log("The cost is " + cost); 
-      
-        console.log(orderItem);
+        //console.log(orderItem);
         connection.query("Select * FROM products where ITEMNUMBER="+ orderItem, function (err, res){
       
           if (answer.stockQuantity > res[0].stock_quantity){
             console.log("Insufficient quantity! Please choose a different quantity")
-          
-
+            connection.end();
           }
           else{
           console.log("Your order has been placed");
           var newQty=(res[0].stock_quantity - answer.stockQuantity);
-          console.log("the new quantity in the database is  " + newQty);
+          var cost = (answer.stockQuantity * res[0].price);
+          console.log("The cost is $ " + cost); 
+      
+          console.log("the new quantity in the database is  " + newQty + "items");
           connection.query("Update Products set stock_quantity = " + newQty,'where itemnumber ='+orderItem,function (err, res){
             var randomNumber = Math.floor(Math.random() * 1000);
-          console.log("Your order number is" + randomNumber) //random number generator )
+          console.log("Your order number is  " + randomNumber) //random number generator )
            console.log("Thanks for your order!"); 
             })
             
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Bamazon
+This program will display the items for sale.  The user will enter the order number they'd like to buy and the quantity. 
+The system will check to see if that quantity is available and then order it for the user, while displaying the order number and the price. 
+
+The program does this by storing the data in the mySQL database. It will contain item number, quantity, item description, and price. 
+Then the node.js will run an inquirer prompt to ask the user how many items they would like, provide the price, cost, order number and then update the database with the final number of items available. 
+Then the program will update the database. 
+
+mySQL and inquirer are required to run this. 
+
+To see a live demo, click here: 
+https://drive.google.com/file/d/1GTPjZkFECxgMHLKbseGMIHAZHNaBMqJL/view?usp=sharing
